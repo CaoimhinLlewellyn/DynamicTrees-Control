ebc0eb0a7d906d7de67ace248bafc922689916d9
ferreusveritas <ferreusveritas@gmail.com>
2018-09-06 13:14:58 -0400

    Basic Mod Structure

---
+++ src/main/java/com/ferreusveritas/dynamictreescontrol/BiomeDataBasePopulator.java
@@ -0,0 +1,13 @@
+package com.ferreusveritas.dynamictreescontrol;
+
+import com.ferreusveritas.dynamictrees.api.worldgen.IBiomeDataBasePopulator;
+
+public class BiomeDataBasePopulator implements IBiomeDataBasePopulator {
+
+	@Override
+	public void populate() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/main/java/com/ferreusveritas/dynamictreescontrol/ModConstants.java
@@ -0,0 +1,19 @@
+package com.ferreusveritas.dynamictreescontrol;
+
+public class ModConstants {
+	
+	public static final String MODID = "dynamictreescontrol";
+	public static final String NAME = "Dynamic Trees Control";
+	public static final String VERSIONDEV = "1.12.2-9.9.9z";
+	public static final String VERSIONAUTO = "@VERSION@";
+	public static final String VERSION = VERSIONAUTO;
+	
+	public static final String AFTER = "after:";
+	public static final String BEFORE = "before:";
+	public static final String NEXT = ";";
+	
+	public static final String DEPENDENCIES
+		= "required-after:" + com.ferreusveritas.dynamictrees.ModConstants.DYNAMICTREES_LATEST
+		;
+	
+}
