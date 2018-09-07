eaf5aa6c9b9fdbb2894728c218065ef42580b93c
ferreusveritas <ferreusveritas@gmail.com>
2018-09-07 09:42:50 -0400

    Convert to registerable IBiomeDataBasePopulator system

--- src/main/java/com/ferreusveritas/dynamictreescontrol/BiomeDataBasePopulator.java
+++ src/main/java/com/ferreusveritas/dynamictreescontrol/BiomeDataBasePopulator.java
@@ -1,11 +1,12 @@
 package com.ferreusveritas.dynamictreescontrol;
 
 import com.ferreusveritas.dynamictrees.api.worldgen.IBiomeDataBasePopulator;
+import com.ferreusveritas.dynamictrees.worldgen.BiomeDataBase;
 
 public class BiomeDataBasePopulator implements IBiomeDataBasePopulator {
 
 	@Override
-	public void populate() {
+	public void populate(BiomeDataBase dbase) {
 		// TODO Auto-generated method stub
 		
 	}
